22085ec5d5cb0804e90acebc36b1d812d21a9743
Baka <batleshemov@gmail.com>
2026-06-05 01:26:39 +0300

    Создание модели представления входа

---
+++ MoneyRec.WPF/ViewModels/AuthorizationViewModels/LoginVM.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoneyRec.Core.Models;
+using MoneyRec.WPF;
+using MoneyRec.WPF.Views;
+using System.Windows;
+
+namespace MoneyRec.WPF.ViewModels.AuthorizationViewModels
+{
+    public partial class LoginVM : ObservableObject
+    {
+        Window _instance;
+        [ObservableProperty]
+        string _login;
+        [ObservableProperty]
+        string _password;
+        User? _user;
+
+        public LoginVM(Window instance)
+        {
+            _instance = instance;
+            Login = "";
+            Password = "";
+        }
+
+        [RelayCommand]
+        void Submit()
+        {
+            if (Verificate())
+            {
+                App.User = _user!;
+                App.Current.MainWindow = new MainWindow();
+                App.Current.MainWindow.Show();
+                _instance.Close();
+            }
+        }
+
+        bool Verificate()
+        {
+            _user = App.Context.Users.Where(u => u.Login == Login && u.Password == Password).FirstOrDefault();
+            return _user is not null;
+        }
+    }
+}
