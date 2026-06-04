d8b51f56d2a186fd9f034d42c40f1b01a055e876
Baka <batleshemov@gmail.com>
2026-06-05 01:26:26 +0300

    Создание общей модели представления авторизации

---
+++ MoneyRec.WPF/ViewModels/AuthorizationViewModels/AuthorizationVM.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MoneyRec.WPF.ViewComponents.LoginWindowComponents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MoneyRec.WPF.ViewModels.AuthorizationViewModels
+{
+    public partial class AuthorizationVM : ObservableObject
+    {
+        Page _loginInstance;
+        Page _registrationInstance;
+        Page _currentInstance;
+        [ObservableProperty]
+        string _buttonName;
+        public AuthorizationVM()
+        {
+            ButtonName = "Регистрация";
+            _loginInstance = new LoginPage();
+            _registrationInstance = new RegistrationPage();
+            _currentInstance = _loginInstance;
+        }
+
+        [RelayCommand]
+        void ChangeInstance()
+        {
+            if (_currentInstance is LoginPage)
+            {
+                ButtonName = "Вход";
+                _currentInstance = _registrationInstance;
+            }
+            else
+            {
+                ButtonName = "Регистрация";
+                _currentInstance = _loginInstance;
+            }
+        }
+    }
+}
