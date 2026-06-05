d4bb375b87f0bc093734de57a4a9ca894903d930
Baka <batleshemov@gmail.com>
2026-06-05 03:08:17 +0300

    стартовое заполнение представлений

--- MoneyRec.WPF/Views/AuthorizationWindow.xaml.cs
+++ MoneyRec.WPF/Views/AuthorizationWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoneyRec.WPF.ViewModels.AuthorizationViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -20,6 +21,7 @@ namespace MoneyRec.WPF.Views
         public LoginWindow()
         {
             InitializeComponent();
+            DataContext = new AuthorizationVM();
         }
     }
 }
--- MoneyRec.WPF/Views/MainWindow.xaml.cs
+++ MoneyRec.WPF/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoneyRec.WPF.ViewModels.MainViewModels;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -20,6 +21,7 @@ namespace MoneyRec.WPF.Views
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainVM();
         }
     }
 }
