056514ecc5098978673fbdb67f315a91682fd741
Baka <batleshemov@gmail.com>
2026-06-08 03:45:32 +0300

    сохранение в json формате

---
+++ MoneyRec.WPF/Services/JSONSaver.cs
@@ -0,0 +1,28 @@
+﻿using MoneyRec.Core.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MoneyRec.WPF.Services
+{
+    public static class JSONSaver
+    {
+        public static readonly string _path = "Data.json";
+        
+
+        static JSONSaver()
+        {
+            if (!File.Exists(_path))
+            {
+                File.Create(_path);
+            }
+        }
+
+        public static void Save()
+        {
+            File.WriteAllText(_path, JsonConvert.SerializeObject(App.Context.Transactions.Where(t => t.UserId == App.User.Id).ToList(), Formatting.Indented));
+        }
+    }
+}
